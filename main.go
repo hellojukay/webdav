76efab2961e632253b95b093d9f42299b24510d6
lic <lic@didapinche.com>
2018-01-30 15:50:26 +0800

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/webdav"
+)
+
+func init() {
+	flag.Parse()
+}
+func main() {
+	var prefix string
+	var port string
+	flag.StringVar(&prefix, "path", "/deployment/apps", "文件夹前缀")
+	flag.StringVar(&port, "port", ":7777", "监听服务和端口")
+	fs := new(webdav.Dir)
+	h := new(webdav.Handler)
+	h.FileSystem = *fs
+	h.Prefix = prefix
+	h.LockSystem = webdav.NewMemLS()
+	//then use the Handler.ServeHTTP Method as the http.HandleFunc
+	http.HandleFunc("/", h.ServeHTTP)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
