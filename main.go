257d26820c2c5af282e2502523cb5192b17efb5a
hellojukay <lic@didapinche.com>
2018-04-25 13:52:57 +0800

    添加用户名和密码的认证

--- main.go
+++ main.go
@@ -7,18 +7,35 @@ import (
 
 	"golang.org/x/net/webdav"
 )
-
-func main() {
-	var port string
-	flag.StringVar(&port, "port", ":7777", "监听服务和端口")
+var bind *string
+var username *string
+var password *string
+func init(){
+	bind = flag.String("bind", ":7777", "监听服务和端口")
+	username = flag.String("username","","用户名")
+	password = flag.String("password","","密码")
 	flag.Parse()
+}
+func auth(w http.ResponseWriter, req *http.Request){
+	u , p , auth := req.BasicAuth()
+	if !auth {
+		w.WriteHeader(403)
+		return
+	}
+	if !(u == *username && p == *password){
+		w.WriteHeader(403)
+		return
+	}
 	var fs webdav.Dir = "/"
 	h := new(webdav.Handler)
 	h.FileSystem = fs
 	h.LockSystem = webdav.NewMemLS()
+	h.ServeHTTP(w,req)
+}
+func main() {
+	http.HandleFunc("/",auth)
 	//then use the Handler.ServeHTTP Method as the http.HandleFunc
-	http.HandleFunc("/", h.ServeHTTP)
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(*port, nil)
 	if err != nil {
 		log.Print(err.Error())
 	}
