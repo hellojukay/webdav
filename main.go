91d32f2a6bf10700f538c6b1bf0de80cf944b785
licong <licong@qianxin.com>
2020-07-24 10:23:03 +0800

    fix go mod

--- main.go
+++ main.go
@@ -7,19 +7,21 @@ import (
 
 	"golang.org/x/net/webdav"
 )
+
 var bind *string
 var h *webdav.Handler
 
 func main() {
 	bind = flag.String("bind", ":7777", "监听服务和端口")
-	
+
 	flag.Parse()
 	var fs webdav.Dir = "/"
 	h := new(webdav.Handler)
 	h.FileSystem = fs
 	h.LockSystem = webdav.NewMemLS()
-	http.HandleFunc("/",h.ServeHTTP)
+	http.HandleFunc("/", h.ServeHTTP)
 	//then use the Handler.ServeHTTP Method as the http.HandleFunc
+	log.Printf("runnin on port %s", *bind)
 	err := http.ListenAndServe(*bind, nil)
 	if err != nil {
 		log.Print(err.Error())
